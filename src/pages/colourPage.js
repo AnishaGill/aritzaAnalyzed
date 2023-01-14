0228d0991293aaf8563313f72de1120170a1162b
Xinyue Cao <ashleyue@students.cs.ubc.ca>
2023-01-14 13:42:03 -0800

    color page

--- src/pages/colourPage.js
+++ src/pages/colourPage.js
@@ -3,7 +3,7 @@ import "./colorPage.css";
 
 export const ColourPage = () => {
     return (
-        <section className="color-class">
+        <section className="">
             <div className="main-title-1">Through all your purchases...</div>
             <div className="main-title-2">One colour came out on top</div>
 
@@ -11,11 +11,13 @@ export const ColourPage = () => {
                 <p className="subtitle-1">Don't worry <br></br>We are also a big fan of</p>
                 <p className="subtitle-2"> Purple</p>
 
-                <div className=""></div>
-
-                <div className="logo3"></div>
+                
                 
             </div>
+
+            <div className=".bg-shape-1"></div>
+
+            <div className="logo3"></div>
             
             
         </section>
