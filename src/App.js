14e411e663d32ca1615d1efaaf5fe54ca52bd6a3
Anisha <gill.anisha@outlook.com>
2023-01-13 21:29:41 -0800

    react routing set-up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main className="container">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to your Aritzia Analysis woo hoo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<welcomePage />}></Route>
+          <Route path="/" element={<brandPage />}></Route>
+          <Route path="/" element={<colourPage />}></Route>
+          <Route path="/" element={<monthMoneyPage />}></Route>
+          <Route path="/" element={<moneyPage />}></Route>
+            {/* included do gooded-ness */}
+          <Route path="/" element={<recommenedPage />}></Route>
+        </Routes>
+        </BrowserRouter>
+        
+    </main>
   );
 }
 
