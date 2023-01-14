b698afdc95fef8eee6fd756ea106baa79a986540
grace02lin@gmail.com <grace@dhcp-128-189-82-113.ubcsecure.wireless.ubc.ca>
2023-01-14 15:28:31 -0800

    further font changes

--- src/pages/endPage.js
+++ src/pages/endPage.js
@@ -6,8 +6,7 @@ export const EndPage = () => {
     return (
         <section>
             <h1 className="firstTextEnd">Liked your report?</h1>
-            <div className="secondTextEnd">so will your friends</div>
-            <div className="thirdTextEnd">share your <a href={yourAnalysis} src="../imgs/yourAnalysis.pngç">Aritzia Analysis</a></div>
+            <div className="secondTextEnd"> <a href={yourAnalysis} target="_blank"  src="../imgs/yourAnalysis.pngç">so will your friends</a> </div>
             <div className="end"></div>
             <div className="logo6"></div>
         </section>
