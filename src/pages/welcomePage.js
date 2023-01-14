aa0823640e7ac9560a4fd53d2fb574cde8a7576b
grace02lin@gmail.com <grace@dhcp-128-189-82-113.ubcsecure.wireless.ubc.ca>
2023-01-14 11:28:37 -0800

    Added welcome page

---
+++ src/pages/welcomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./welcomePage.css";
+
+export const WelcomePage = () => {
+    return (
+        <main>
+        <div className="desc">Welcome Grace, to your Yearly Luxury report</div>
+        <div className="main-img"></div>
+        </main>
+
+    );
+}
