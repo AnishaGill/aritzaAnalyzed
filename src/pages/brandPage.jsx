b6fe88dcc3f4c295a1216494d3d6c3b8497c067b
Anisha <gill.anisha@outlook.com>
2023-01-14 12:12:34 -0800

    brand page started

---
+++ src/pages/brandPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import css from brand page .css 
+
+
+
+export const BrandPage = () => {
+    return ( 
+        <main>
+            <div>The brands that won your love this year: </div>;
+            <div className="main-img"> 
+            {/* <img src={}> </img> */}
+            </div>
+        </main>
+
+    
+    
+    );
+}
